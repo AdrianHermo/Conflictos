8777a9aaca659087446b07396f27eff4c9c47913
Adrian <ahermoabalde@danielcastelao.org>
2026-01-13 11:28:00 +0100

    Codigo calculadora

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+# 1 Programa de ejemplo
+def sumar(a, b):
+    # 3 Comentario de relleno
+    # 4 Otro comentario de relleno
+    resultado = a - b      # 5 ← Operación inicial (NO suma)
+    return resultado       # 6
+
+num1 = 5
+num2 = 3
+
+resultado_final = sumar(num1, num2)
+print("El resultado es:", resultado_final)
